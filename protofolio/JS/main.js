401513d8eb1e07993db223ac73fd2d1afdaaf243
Gasser Zayed <gasserzayed.work0@gmail.com>
2025-08-17 16:08:52 +0300

    Add files via upload

---
+++ protofolio/JS/main.js
@@ -0,0 +1,14 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target); 
+        }
+    });
+    });
+
+reveals.forEach(reveal => {
+    observer.observe(reveal);
+});
